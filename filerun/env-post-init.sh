9e6bf21b157ea6a8adbf3effb114d0c5a11567ec
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2020-12-25 22:49:47 +0300

    Update env-post-init.sh

--- filerun/env-post-init.sh
+++ filerun/env-post-init.sh
@@ -1,4 +1,4 @@
 #docker run -it --entrypoint bash -v /download:/download phenomrascalov/centos7.7
 git clone https://github.com/phenomrascalov/katacoda-scenarios.git
 cd katacoda-scenarios
-chmod +x check && ./check.sh
+chmod +x check.sh && ./check.sh
