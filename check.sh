3e86ffb3b95a6ef418ad9ab66d218be37f3acf4c
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2020-12-25 23:43:00 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,13 +1,12 @@
 for (( c=1; c<=100; c++ ))
 do  
-curl http://localhost || var=0
-if [ $var == false ]
+curl http://localhost || var=""
+if test -z "$var"
 then
       echo "Please Wait"
       sleep 2
       clear
 else
-      echo "https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com"
       break
  fi
 
