6bc1d9faf5e509d77fa3b6f872f18cffd27fccb2
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2020-12-25 23:32:32 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,11 @@
+for (( c=1; c<=100; c++ ))
+do  
 curl http://localhost || var=""
 if test -z "$var" 
 then
-      echo "\$var is empty"
+      echo "Please Wait"
 else
-      echo "\$var is NOT empty"
+      echo "https://[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com"
+      break
 fi
+done
