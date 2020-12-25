82a3ba562c6ca6793c4b415bccbe96b5cf4a9015
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2020-12-26 00:04:44 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,13 +1,14 @@
-for (( c=1; c<=100; c++ ))
-do  
+c=0
+while [  $c -lt 100 ]; do
 var=$(curl -Is http://localhost | head -1)
 if test -z $var
 then
       echo "Please Wait"
+	  let c=c+1 
       sleep 1
       clear
 else
       echo "Ready"
       break
- fi
+fi
 done
