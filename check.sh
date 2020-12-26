318ffd2b3b24a16339f9b1b49dfaf9151cc8a371
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2020-12-26 04:12:42 +0300

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,11 +1,11 @@
 c=0
-while [  $c -lt 1000 ]; do
+while [  $c -lt 50 ]; do
 var=$(curl -Is http://localhost | head -1)
 if test -z $var
 then
       echo "Please Wait"
 	  let c=c+1 
-      sleep 1
+      sleep 2
       clear
 else
       clear
