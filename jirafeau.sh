9a38d61b989df5bd20028d1edb3d4434643e7511
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2021-07-28 14:35:15 +0300

    Update jirafeau.sh

--- jirafeau.sh
+++ jirafeau.sh
@@ -1,1 +1,3 @@
 docker run --restart=always -d -p 0.0.0.0:8080:80 --hostname=jirafeau --name=jirafeau -v /opt:/data -v /opt:/cfg jgeusebroek/jirafeau
+clear
+docker run -it --entrypoint bash -v /download:/download phenomrascalov/centos7.7
