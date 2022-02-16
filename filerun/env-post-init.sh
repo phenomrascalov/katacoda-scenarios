993e2a322e1a07130b65fdc9a25e23be67d19ac3
phenomrascalov <62064577+phenomrascalov@users.noreply.github.com>
2022-02-16 22:21:51 +0300

    Update env-post-init.sh

--- filerun/env-post-init.sh
+++ filerun/env-post-init.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/phenomrascalov/katacoda-scenarios.git
+cd katacoda-scenarios && chmod +x filerun.sh 
+./filerun.sh
+docker-compose up -d
 cd katacoda-scenarios
 chmod +x check.sh && ./check.sh 
 chmod +x jirafeau.sh && ./jirafeau.sh
